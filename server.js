ea223421d19564923fb223a03e3ad984b3d7afaf
Catalin <csiegling@gmail.com>
2022-06-05 14:29:48 -0500

    working api

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
-const PORT = 8000
 const cors = require('cors')
+const PORT = 8000
 
 app.use(cors())
 
 const tea = {
     'oolong': {
+        'type': 'black',
+        'origin': 'Yo mommas HOuse',
         'waterTemp': 200,
-        'caffeineLevel': 'over 9000',
+        'steepTimeSeconds': 180,
+        'caffinated': true,
+        'flavor': 'delicious'
+    },
+    'matcha': {
         'type': 'green',
-        'flavor': 'earthy'
-    
-},
-'unknown': {
-    'waterTemp': 0,
-        'caffeineLevel': 'unknown',
-            'type': 'unknown',
-                'flavor': 'unknown'
-},
-
-'matcha': {
-    'waterTemp': 180,
-        'caffeineLevel': 'over 5',
-            'type': 'black',
-                'flavor': 'heavenly'
-}
+        'origin': 'Yo mommas HOuse',
+        'waterTemp': 200,
+        'steepTimeSeconds': 180,
+        'caffinated': false,
+        'flavor': 'delicious'
+    },
+    'unknown': {
+        'type': 'unknown',
+        'origin': 'unknown',
+        'waterTemp': 0,
+        'steepTimeSeconds': 0,
+        'caffinated': false,
+        'flavor': 'unknown'
+    }
 }
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is running on port ${PORT}`);
-})
-app.get('/api:name', (request, response) => {
+
+app.get('/api/:name', (request, response) => {
     const teaName = request.params.name.toLowerCase()
     if (tea[teaName]) {
         response.json(tea[teaName])
     } else {
         response.json(tea['unknown'])
     }
-    response.json(tea)
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on port ${PORT}.`)
 })
